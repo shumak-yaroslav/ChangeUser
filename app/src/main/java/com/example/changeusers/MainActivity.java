2e39954217568c441b61058e4de7054352e1cbe1
Yaroslav <56405461+yani-eniz@users.noreply.github.com>
2020-04-30 17:27:47 +0300

    add smth

--- app/src/main/java/com/example/changeusers/MainActivity.java
+++ app/src/main/java/com/example/changeusers/MainActivity.java
@@ -33,25 +33,62 @@ ComponentName componentName;
         setContentView(R.layout.activity_main);
 
         btn1 = findViewById(R.id.enable);
-
-        final UserManager um = (UserManager) getSystemService(USER_SERVICE);
-        assert um != null;
-        final List<UserHandle> userProfiles = um.getUserProfiles();
-
+        btn2 = findViewById(R.id.disable);
 
         devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
         componentName = new ComponentName(MainActivity.this,Controller.class);
 
+        final boolean active = devicePolicyManager.isAdminActive(componentName);
+
+        if(active){
+            btn1.setText("Disable");
+            btn2.setVisibility(View.VISIBLE);
+        }else{
+            btn1.setText("Enable");
+            btn2.setVisibility(View.GONE);
+        }
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                devicePolicyManager.switchUser(componentName,);
+                boolean active = devicePolicyManager.isAdminActive(componentName);
+                if(active){
+                    devicePolicyManager.removeActiveAdmin(componentName);
+                    btn1.setText("Enable");
+                    btn2.setVisibility(View.GONE);
+                }else{
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,componentName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"You should enable the app!");
+                    startActivityForResult(intent,RESULT_ENABLE);
+
+                }
             }
         });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                devicePolicyManager.lockNow();
 
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        switch (requestCode){
+            case RESULT_ENABLE:
+                if(resultCode==Activity.RESULT_OK){
+                    btn1.setText("Disable");
+                    btn2.setVisibility(View.VISIBLE);
+                }else {
+                    Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }

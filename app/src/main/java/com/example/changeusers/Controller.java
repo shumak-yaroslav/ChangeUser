e8e315066695ce96e1bdb80a18e1bcdca8d5f233
Yaroslav <56405461+yani-eniz@users.noreply.github.com>
2020-05-04 17:32:51 +0300

    add some version

--- app/src/main/java/com/example/changeusers/Controller.java
+++ app/src/main/java/com/example/changeusers/Controller.java
@@ -3,6 +3,7 @@ package com.example.changeusers;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserHandle;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,5 +18,15 @@ public class Controller extends DeviceAdminReceiver {
     @Override
     public void onDisabled(@NonNull Context context, @NonNull Intent intent) {
         Toast.makeText(context,"Disabled!",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUserSwitched(@NonNull Context context, @NonNull Intent intent, @NonNull UserHandle switchedUser) {
+        Toast.makeText(context,"Switched!",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUserAdded(@NonNull Context context, @NonNull Intent intent, @NonNull UserHandle newUser) {
+        Toast.makeText(context,"Added!",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/changeusers/MainActivity.java
+++ app/src/main/java/com/example/changeusers/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.changeusers;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,7 +10,10 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.view.View;
@@ -17,78 +21,70 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.List;
-
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-Button btn1,btn2;
-
-static final int RESULT_ENABLE = 1;
-DevicePolicyManager devicePolicyManager;
-ComponentName componentName;
 
 
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchUser();
 
-        btn1 = findViewById(R.id.enable);
-        btn2 = findViewById(R.id.disable);
-
-        devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        componentName = new ComponentName(MainActivity.this,Controller.class);
-
-        final boolean active = devicePolicyManager.isAdminActive(componentName);
-
-        if(active){
-            btn1.setText("Disable");
-            btn2.setVisibility(View.VISIBLE);
-        }else{
-            btn1.setText("Enable");
-            btn2.setVisibility(View.GONE);
-        }
-
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean active = devicePolicyManager.isAdminActive(componentName);
-                if(active){
-                    devicePolicyManager.removeActiveAdmin(componentName);
-                    btn1.setText("Enable");
-                    btn2.setVisibility(View.GONE);
-                }else{
-                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,componentName);
-                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"You should enable the app!");
-                    startActivityForResult(intent,RESULT_ENABLE);
-
-                }
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                devicePolicyManager.lockNow();
-
-            }
-        });
 
     }
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
-        switch (requestCode){
-            case RESULT_ENABLE:
-                if(resultCode==Activity.RESULT_OK){
-                    btn1.setText("Disable");
-                    btn2.setVisibility(View.VISIBLE);
-                }else {
-                    Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show();
-                }
-                return;
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void switchUser() {
+        DevicePolicyManager dpm =  (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        ComponentName admin = new ComponentName(MainActivity.this,Controller.class);
+
+        // If device admin not active, take user to settings
+        if (!dpm.isAdminActive(admin)) {
+            Intent activateDeviceAdminIntent =  new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            activateDeviceAdminIntent.putExtra(
+                    DevicePolicyManager.EXTRA_DEVICE_ADMIN,
+                    admin
+            );
+
+            startActivityForResult(activateDeviceAdminIntent, 1);
+            return;
         }
-        super.onActivityResult(requestCode, resultCode, data);
+
+        if (dpm.isDeviceOwnerApp(admin.getPackageName())) {  // Return false if app run as secondary user
+            List<UserHandle> secondaryUsers = dpm.getSecondaryUsers(admin);
+            if (!secondaryUsers.isEmpty()) {  // Switch to secondary user
+                dpm.switchUser(admin, secondaryUsers.get(0));
+            } else {    // Create and switch to new user
+                Set<String> identifiers = dpm.getAffiliationIds(admin);
+                if (identifiers.isEmpty()) {
+                    identifiers.add(UUID.randomUUID().toString());
+                    dpm.setAffiliationIds(admin, identifiers);
+                }
+                PersistableBundle adminExtras = new PersistableBundle();
+                adminExtras.putString("AFFILIATION_ID_KEY", identifiers.iterator().next());
+                try {
+                    UserHandle newUser = dpm.createAndManageUser(
+                            admin,
+                            "Foo",
+                            admin,
+                            adminExtras,
+                            DevicePolicyManager.MAKE_USER_EPHEMERAL |
+                                    DevicePolicyManager.SKIP_SETUP_WIZARD);
+                    dpm.switchUser(admin, newUser);
+                } catch (UserManager.UserOperationException e) {
+                    Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        }
     }
 
 }
